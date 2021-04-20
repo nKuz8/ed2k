3d82492ca316d8ae2ff69048bcde5b8a992892fd
02-16 Никита К <nikitaosik@gmail.com>
2021-04-20 19:59:05 +0300

    Working thing

--- ed2k/main.py
+++ ed2k/main.py
@@ -1,7 +1,6 @@
-import MD4
-
-
 import struct
+import time
+import random
 
 
 class MD4:
@@ -23,11 +22,6 @@ class MD4:
         msg += struct.pack("<Q", ml)
 
         self._process([msg[i: i + 64] for i in range(0, len(msg), 64)])
-
-    # def __repr__(self):
-    #     if self.msg:
-    #         return f"{self.__class__.__name__}({self.msg:s})"
-    #     return f"{self.__class__.__name__}()"
 
     def __str__(self):
         return self.hexdigest()
@@ -103,11 +97,6 @@ class ED2K:
 
         self.compute_hash()
 
-    def __repr__(self):
-        if self.msg:
-            return f"{self.__class__.__name__}({self.msg:s})"
-        return f"{self.__class__.__name__}()"
-
     def compute_hash(self):
         pre_hash = b""
         for i in range(0, len(self.msg), 9728000):
@@ -118,30 +107,61 @@ class ED2K:
     def hexdigest(self):
         return "".join(f"{value:02x}" for value in self.hash)
 
+    def __str__(self):
+        return self.hexdigest()
+
 
 def main():
-    # Import is intentionally delayed.
-    import sys
+    """1 block hash time test"""
+    time_start = time.time()
+    block = b""
+    ED2K(block)
+    print("1 block hash time: {}".format(time.time() - time_start))
 
-    if len(sys.argv) > 1:
-        with open(sys.argv[1], 'rb') as input:
-            message = input.read()
-            print(ED2K(message).hexdigest())
-    else:
-        messages = [b"", b"The quick brown fox jumps over the lazy dog", b"zeros.txt"]
-        known_hashes = [
-            "31d6cfe0d16ae931b73c59d7e0c089c0",
-            "1bee69a46ba811185c194762abaeae90",
-            "59495f401f014f2522c4eae3c9a2b1f8",
-        ]
+    """10^3 block hash time test"""
+    time_start = time.time()
+    block = b""
+    for i in range(10 ** 3):
+        block = ED2K(block).hash
+    print("10^3 block hash time: {}".format(time.time() - time_start))
 
-        print("Testing the MD4 class.")
-        print()
+    """10^6 block hash time test"""
+    time_start = time.time()
+    block = b""
+    for i in range(10 ** 6):
+        block = ED2K(block).hash
+    print("10^6 block hash time: {}".format(time.time() - time_start))
 
-        for message, expected in zip(messages, known_hashes):
-            print("Message: ", message)
-            print("Expected:", expected)
-            print("Actual:  ", ED2K(message).hexdigest())
+    """1 mb file hash time test"""
+    with open("test/ot_1mb.txt", 'rb') as file:
+        time_start = time.time()
+        ED2K(file.read())
+        print("1 mb hash time: {}".format(time.time() - time_start))
+
+    """100 mb file hash time test"""
+    with open("test/ot_100mb.txt", 'rb') as file:
+        time_start = time.time()
+        ED2K(file.read())
+        print("100 mb hash time: {}".format(time.time() - time_start))
+
+    """1 Gb file hash time test"""
+    with open("test/ot_1Gb.txt", 'rb') as file:
+        time_start = time.time()
+        ED2K(file.read())
+        print("1 Gb hash time: {}".format(time.time() - time_start))
+
+    """Test on known MD4 hashes"""
+    messages = [b"", b"The quick brown fox jumps over the lazy dog", b"The quick brown fox jumps over the lazy cog"]
+    for message in messages:
+        print("Actual message: {}\n hash: {}".format(message, ED2K(message).hexdigest()))
+
+    """Test of known ED2K hashes"""
+    expected = ["d7def262a127cd79096a108e7a9fc138", "194ee9e4fa79b2ee9f8829284c466051", "9a68abb94d13f1e6ea13e968279652d7"]
+    for i in range(3):
+        with open("test/zeros_{}.txt".format(i + 1), 'rb') as file:
+            print("Hashing {}".format(file.name))
+            print("Expected: {}".format(expected[i]))
+            print("Actual: {}".format(ED2K(file.read()).hexdigest()))
             print()
 
 
